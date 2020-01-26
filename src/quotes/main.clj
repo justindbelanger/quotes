bb4ac795d7b3095a6ca3c37fcc8f73113ab7ee16
Justin Belanger <justindbelanger@outlook.com>
2020-01-26 11:36:27 -0800

    Use middleware for Riemann and random quote

--- src/quotes/main.clj
+++ src/quotes/main.clj
@@ -5,9 +5,47 @@
             [reitit.ring]
             [riemann.client]))
 
+;; * Quotes
+
+(def quotes
+  [{:text   "If we're not solving problems, what are we doing?"
+    :person "Rich Hickey"}
+   {:text   "Good design has to account for all of the pieces, not just some."
+    :person "Stuart Halloway"}
+   {:text   "I’m never going back to a non-LISP. Life is too short to learn fussy language grammars with tons of special cases."
+    :person "Gene Kim"}])
+
+(defn wrap-random-quote
+  "Provides a (pseudo)randomized quote to display on the page."
+  [handler]
+  (fn [request]
+    (handler (assoc request :quote (rand-nth quotes)))))
+
+;; * Sending events to Riemann
+
+(def riemann-client (riemann.client/tcp-client {:host "0.0.0.0"
+                                                :port 5555}))
+
+(defn wrap-riemann-service-time
+  "Sends time to service each request to Riemann."
+  [handler]
+  (fn [request]
+    (let [start    (System/nanoTime)
+          response (handler request)
+          end      (System/nanoTime)
+          duration (- end start)]
+      (-> riemann-client
+          (riemann.client/send-event {:service "quotes.http.index.service-time"
+                                      :metric  duration
+                                      :state   "ok"})
+          (deref 5000 ::timeout))
+      response)))
+
+;; * HTTP handlers
+
 (defn index-page-handler
-  [get-quote _request]
-  (let [{:keys [text person] :as _quote} (get-quote)]
+  [request]
+  (let [{:keys [text person]} (:quote request)]
     {:status 200
      :body   (hiccup.core/html
               (hiccup.page/html5
@@ -19,33 +57,15 @@
                 [:p [:span "\""] [:strong text] [:span "\""]]
                 [:p [:span " - "] person]]))}))
 
-(def quotes
-  [{:text   "If we're not solving problems, what are we doing?"
-    :person "Rich Hickey"}
-   {:text   "Good design has to account for all of the pieces, not just some."
-    :person "Stuart Halloway"}
-   {:text   "I’m never going back to a non-LISP. Life is too short to learn fussy language grammars with tons of special cases."
-    :person "Gene Kim"}])
-
-(def riemann-client (riemann.client/tcp-client {:host "0.0.0.0"
-                                                :port 5555}))
-
 (def app
   (reitit.ring/ring-handler
    (reitit.ring/router
-    ["/" {:get (fn [request]
-                 (let [start    (System/nanoTime)
-                       response (index-page-handler (fn [] (rand-nth quotes))
-                                                    request)
-                       end      (System/nanoTime)
-                       duration (- end start)]
-                   (-> riemann-client
-                       (riemann.client/send-event {:service "quotes.http.index.service-time"
-                                                   :metric  duration
-                                                   :state   "ok"})
-                       (deref 5000 ::timeout))
-                   response))}])
+    ["/" {:get        index-page-handler
+          :middleware [[wrap-riemann-service-time]
+                       [wrap-random-quote]]}])
    (reitit.ring/create-default-handler)))
+
+;; * HTTP server
 
 (defonce *server (atom nil))
 
@@ -57,6 +77,8 @@
   []
   (.close @*server)
   (reset! *server nil))
+
+;; * App entry point
 
 (defn -main [& {:keys [port]
                 :or   {port 8080}}]
